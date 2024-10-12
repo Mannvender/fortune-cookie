22b91cba308652dbee650152495085c0090af655
mannvender <mannvender001@gmail.com>
2024-10-12 19:41:59 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./login/Login.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<div className="text-blue-500 text-fuchsia-800">home</div>}
+        />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/contexts/loader.js
@@ -0,0 +1,17 @@
+import React, { createContext, useContext, useState } from "react";
+import Loader from "../loader/Loader";
+
+const LoaderContext = createContext();
+
+function LoaderContextProvider({ children }) {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <LoaderContext.Provider value={{ loading, setLoading }}>
+      {loading && <Loader />}
+      {children}
+    </LoaderContext.Provider>
+  );
+}
+
+export { LoaderContext, LoaderContextProvider };
